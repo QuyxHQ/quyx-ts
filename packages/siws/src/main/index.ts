01a29238f9c317779cff544b00b3684af408872a
Adedeji Morifeoluwa <141498118+iamthe-nerdyDev@users.noreply.github.com>
2024-03-20 21:24:57 +0100

    changes

--- packages/siws/src/main/index.ts
+++ packages/siws/src/main/index.ts
@@ -1,7 +1,6 @@
 import bs58 from "bs58";
 import nacl from "tweetnacl";
 import { QuyxSIWSProps } from "../types";
-import { dateUTC } from "../utils/helper";
 
 export interface QuyxSIWS {
   prepare(): Uint8Array;
@@ -9,13 +8,13 @@ export interface QuyxSIWS {
 }
 
 export class QuyxSIWS implements QuyxSIWS {
-  private domain: any;
-  private address: any;
-  private nonce: any;
-  private statement: any;
-  private chainId: any;
-  private issuedAt: any;
-  private expirationTime: any;
+  domain: any;
+  address: any;
+  nonce: any;
+  statement: any;
+  chainId: any;
+  issuedAt: any;
+  expirationTime: any;
 
   constructor(props: QuyxSIWSProps) {
     this.domain = props.domain || new URL(window.location.href).host;
@@ -30,7 +29,7 @@ export class QuyxSIWS implements QuyxSIWS {
   }
 
   prepare() {
-    const msg = `${this.domain}${this.statement}\n\nNonce: ${this.nonce}\nChain ID: ${this.chainId}\nIssued At: ${this.issuedAt}\nExpiration Time: ${this.expirationTime}`;
+    const msg = `${this.domain}\n${this.statement}\n\nNonce: ${this.nonce}\nChain ID: ${this.chainId}\nIssued At: ${this.issuedAt}\nExpiration Time: ${this.expirationTime}`;
     return new TextEncoder().encode(msg);
   }
 
@@ -38,12 +37,13 @@ export class QuyxSIWS implements QuyxSIWS {
     const msg = this.prepare();
     const pubKeyUint8 = bs58.decode(this.address);
 
-    const UTC_now = dateUTC().getTime();
-    const UTC_issuedAt = dateUTC(this.issuedAt).getTime();
-    const UTC_expirationTime = dateUTC(this.expirationTime).getTime();
+    // TODO: date check logic!
+    // const UTC_now = dateUTC().getTime();
+    // const UTC_issuedAt = dateUTC(this.issuedAt).getTime();
+    // const UTC_expirationTime = dateUTC(this.expirationTime).getTime();
 
-    if (UTC_now < UTC_issuedAt) throw new Error("issuedAt is in future");
-    if (UTC_now >= UTC_expirationTime) throw new Error("expired! request a new message");
+    // if (UTC_now < UTC_issuedAt) throw new Error("issuedAt is in future");
+    // if (UTC_now >= UTC_expirationTime) throw new Error("expired! request a new message");
 
     return nacl.sign.detached.verify(msg, signature, pubKeyUint8);
   }
